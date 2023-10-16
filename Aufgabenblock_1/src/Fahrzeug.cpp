d081beb26ad0afb9e828d4c9e078c9e245538dd4
Lukas Lohmar <lukas.lohmar@fenjax.com>
2023-10-16 16:24:16 +0200

    Unterpunkt 6

--- Aufgabenblock_1/src/Fahrzeug.cpp
+++ Aufgabenblock_1/src/Fahrzeug.cpp
@@ -7,12 +7,29 @@
 
 #include "Fahrzeug.h"
 
-Fahrzeug::Fahrzeug() {
-	// TODO Auto-generated constructor stub
+int Fahrzeug::p_iIDCnt = 0;
+
+Fahrzeug::Fahrzeug()
+	: p_sName(""), p_iID(p_iIDCnt++), p_dMaxGeschwindigkeit(0), p_dGesamtZeit(0), p_dZeit(0)
+{
+	std::cout << "New Fahrzeug generated: " << p_sName << " - " << p_iID << std::endl;
 
 }
 
-Fahrzeug::~Fahrzeug() {
-	// TODO Auto-generated destructor stub
+Fahrzeug::Fahrzeug(std::string initName)
+	: p_sName(initName), p_iID(p_iIDCnt++), p_dMaxGeschwindigkeit(0), p_dGesamtZeit(0), p_dZeit(0)
+{
+	std::cout << "New Fahrzeug generated: " << p_sName << " - " << p_iID << std::endl;
 }
+
+Fahrzeug::Fahrzeug(std::string initName, double initMaxVelo)
+	: p_sName(initName), p_iID(p_iIDCnt++), p_dMaxGeschwindigkeit(initMaxVelo >= 0 ? initMaxVelo : 0), p_dGesamtZeit(0), p_dZeit(0)
+{
+}
+
+Fahrzeug::~Fahrzeug()
+{
+	std::cout << "Fahrzeug deleted: " << p_sName << " - " << p_iID << std::endl;
+}
+
 
--- Aufgabenblock_1/src/main.cpp
+++ Aufgabenblock_1/src/main.cpp
@@ -1,8 +1,48 @@
+#include <vector>
+#include <memory>
+
 #include "Fahrzeug.h"
 
 
+void vAufgabe_1()
+{
+	Fahrzeug F1;
+	Fahrzeug* F2 = new Fahrzeug("PKW");
+	delete F2; // destruction of F2
+
+	std::unique_ptr<Fahrzeug> p1 = std::make_unique<Fahrzeug>();
+	std::unique_ptr<Fahrzeug> p2 = std::make_unique<Fahrzeug>("PKW");
+	std::shared_ptr<Fahrzeug> p3 = std::make_shared<Fahrzeug>();
+	std::shared_ptr<Fahrzeug> p4 = std::make_shared<Fahrzeug>("PKW");
+
+	std::cout << "p3.use_count: " << p3.use_count() << std::endl;
+	std::shared_ptr<std::shared_ptr<Fahrzeug>> p5 = std::make_shared<std::shared_ptr<Fahrzeug>>(p3);
+	std::cout << "p3.use_count: " << p3.use_count() << std::endl;
+
+	std::cout << std::endl << "Vector: " << std::endl;
+
+	std::vector<std::unique_ptr<Fahrzeug>> vec;
+
+	vec.push_back(move(p1));
+	vec.push_back(move(p2));
+
+	vec.clear(); //destruct p1, p2
+
+	std::cout << std::endl << "2nd Vector" << std::endl;
+
+	std::cout << "use_counts: " << "p3: " << p3.use_count() << " p4: " << p4.use_count() << std::endl;
+
+	std::vector<std::shared_ptr<Fahrzeug>> vec2;
+	vec2.push_back(p3);
+	vec2.push_back(move(p4));
+
+	std::cout << "use_counts: " << "p3: " << vec2.at(0).use_count() << " p4: " << vec2.at(1).use_count() << std::endl;
+
+	std::cout << std::endl << "Finish function" << std::endl;
+} // destruction of everything else
 
 int main()
 {
+	vAufgabe_1();
 	return 0;
 }
